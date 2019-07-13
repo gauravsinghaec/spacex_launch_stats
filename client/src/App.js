43ebf4a1e3cc7e464be0ffb3a4cf8914ed03b36c
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2019-07-13 15:25:07 +0530

    style: logo height and width

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt='spacex logo' />
+      <img
+        src={logo}
+        alt='spacex logo'
+        style={{width: 300, height: 200}}
+        />
     </div>
   );
 }

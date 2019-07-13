5d3c0f8c06a159400d22e43de17c8dbe5074298b
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2019-07-13 16:18:33 +0530

    feat: create the client and pass it to app via ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import logo from './logo.png'
 import './App.css';
 
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql"
+});
+
 function App() {
   return (
-    <div className="App">
-      <img
-        src={logo}
-        alt='spacex logo'
-        style={{width: 300, height: 200}}
-        />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <img
+          src={logo}
+          alt='spacex logo'
+          style={{width: 300, height: 200}}
+          />
+      </div>
+    </ApolloProvider>
   );
 }
 
